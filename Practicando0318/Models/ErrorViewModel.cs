5c87e7e054a3cec3c37e4aac5868b1acfce1c91e
Cristian Daniel Colocho López <colochocristian@outlook.com>
2025-03-18 21:20:13 -0600

    Agregar archivos de proyecto.

---
+++ Practicando0318/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practicando0318.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
